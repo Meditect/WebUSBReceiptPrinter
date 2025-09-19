99de3c1b82c270d07310fa290a7714f5039e521c
Roland <roland.paire@gmx.fr>
2025-09-19 15:14:41 +0200

    remove: webusb-receipt-printer library
    Removes the webusb-receipt-printer library due to its unmaintained state.
    This change eliminates the library and all associated files.

--- dist/event-emitter.d.ts
+++
@@ -1,7 +0,0 @@
-type EventsMap = Record<string, unknown[]>;
-export declare class EventEmitter<E extends EventsMap> {
-    private events;
-    on<K extends keyof E>(event: K, listener: (...args: E[K]) => void): void;
-    emit<K extends keyof E>(event: K, ...args: E[K]): void;
-}
-export {};
